3edd8b7ea72fa1c7a72f954c40626f31560e35b3
tantan5 <jojo1938043@gmail.com>
2016-10-02 15:57:11 +0000

    マッチングロジックの追加。formからのパラメータ取得がまだ出来てません。#14 @1h

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TopControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "画像イメージと旅行先の選定ロジック" do
+    controller = TopController.new
+    output_list =["泉岳寺（赤穂浪士の墓）","碓氷第三橋梁","足尾銅山","東京ドームシティ ASOBono！","インスタントラーメン博物館","ワープステーション江戸","釣船茶屋 ざうお","犬吠埼","巨大迷路パラディアム","目黒寄生虫館"]
+    (1..10).each{|input_no|
+      assert_equal(output_list[input_no-1],controller.match_by_image(input_no))
+    }
+  end
 end
